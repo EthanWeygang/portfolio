635091fadccaf26e8ff887308a5598d69582b4d1
Ethan Weygang <ethanweygang@gmail.com>
2025-08-13 14:38:19 +0100

    Added colour

--- src/components/HoverWord.tsx
+++ src/components/HoverWord.tsx
@@ -7,7 +7,7 @@ function HoverWord({ word }: HoverWordProps){
     return(
         <div className="flex">
             {word.split('').map((char, i) => (
-                <div className="transition duration-300 ease-in-out hover:scale-150 text-6xl" key={i}>{char}</div>
+                <div className="transition duration-300 ease-in-out hover:scale-150 hover:text-orange1 text-6xl" key={i}>{char}</div>
             ))}
         </div>
     )
